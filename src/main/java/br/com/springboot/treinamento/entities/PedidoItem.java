c6fbb7ad84f6913a76c7dfbdee28d6b95dc3b329
Vanderlei Kleinschmidt <vanderleik@yahoo.com.br>
2023-03-17 16:27:36 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/br/com/springboot/treinamento/entities/PedidoItem.java
@@ -0,0 +1,86 @@
+package br.com.springboot.treinamento.entities;
+
+import br.com.springboot.treinamento.entities.pk.PedidoItemPK;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "pedido_item")
+public class PedidoItem implements Serializable {
+    private static final long serialVersionId = 1L;
+
+    @EmbeddedId
+    private PedidoItemPK id;
+    private Integer quantidade;
+    private Double preco;
+
+    public PedidoItem() {
+    }
+
+    public PedidoItem(Pedido pedido, Produto produto, Integer quantidade, Double preco) {
+        id.setPedido(pedido);
+        id.setProduto(produto);
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    /**
+     * Fiz na mão os getter e setter do pedido e produto
+     */
+    public Pedido getPedido() {
+        return id.getPedido();
+    }
+
+    public void setPedido(Pedido pedido) {
+        id.setPedido(pedido);
+    }
+
+    public Produto getProduto() {
+        return id.getProduto();
+    }
+
+    public void setProduto(Produto produto) {
+        id.setProduto(produto);
+    }
+
+    public PedidoItemPK getId() {
+        return id;
+    }
+
+    public void setId(PedidoItemPK id) {
+        this.id = id;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PedidoItem)) return false;
+        PedidoItem that = (PedidoItem) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
